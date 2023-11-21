4c7ebc9833a4d624f453cc8e3d118045d7fc8860
Kandeswari S <38218378+Kandeswari@users.noreply.github.com>
2023-11-21 21:56:17 +0530

    Update Projects.js

--- portfolio_template-master/src/components/Projects.js
+++ portfolio_template-master/src/components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
                 <div className="projects__card">
                     <h3>Portfolio Site</h3>
                     <img src={PortfolioSite} alt="Portfolio Site Page" />
-                    <p>I designed and built my portfolio site from the ground up, tailoring it to my unique style.I took a hands-on approach to create my portfolio site.
+                    <p>I designed and built my portfolio site from the ground up, tailoring it to my unique style.I took a hands-on approach to creating my portfolio site.
                     </p>
 
                     <a href="https://mute-bonus-4684.on.fleek.co/" target="_blank" className="button">Site</a>
@@ -26,7 +26,7 @@ const Projects = () => {
                     <img src={compound} alt="Compound Landing Page" />
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+perferendis ratione rerum dolorem.
                     </p>
 
                     <a href="https://compound.finance/" target="_blank" className="button">Site</a>
@@ -38,7 +38,7 @@ const Projects = () => {
                     <img src={aave} alt="Aave Landing Page" />
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+perferendis ratione rerum dolorem.
                     </p>
 
                     <a href="https://aave.com/" target="_blank" className="button">Site</a>
