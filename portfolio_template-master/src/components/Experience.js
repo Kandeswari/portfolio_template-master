2f3e067cddb9fd19ef23d55cf5be9a535b7c679c
Kandeswari S <38218378+Kandeswari@users.noreply.github.com>
2023-11-21 21:53:10 +0530

    Update Experience.js

--- portfolio_template-master/src/components/Experience.js
+++ portfolio_template-master/src/components/Experience.js
@@ -4,24 +4,23 @@ const Experience = () => {
             <h2>Experience</h2>
 
             <ul>
-                <li>
+               {/*  <li>
                     <i>Clayton State University</i>, 2014-2018
                     <ul>
                         <li>Graduated with 3.00 GPA</li>
                     </ul>
                 </li>
                 <li>
-                    <i>TechFlow</i> - <strong>Blockchain Developer Intern</strong>, 2018-2019
+                    <i>TechFlow</i> <strong>Blockchain Developer Intern</strong>, 2018-2019
                     <ul>
                         <li>Studied DApp development</li>
                         <li>Assisted Research &amp; Development on Web3 Products</li>
                     </ul>
-                </li>
+                </li> */}
                 <li>
-                    <i>SpiderChain</i> - <strong>Blockchain Developer</strong>, 2019-2022
+                    <i></i> - <strong>Blockchain Developer</strong>, 2022-2023
                     <ul>
-                        <li>Contributed to smart contract development on client's platform</li>
-                        <li>Peer reviewed &amp; Audited Solidity code</li>
+                        <li>Contributed to smart contract development on test nft projects</li>
                     </ul>
                 </li>
             </ul>
