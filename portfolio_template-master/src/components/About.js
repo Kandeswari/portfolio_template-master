cc89a67f8d6c69944422dc066a72af2355241724
Kandeswari S <38218378+Kandeswari@users.noreply.github.com>
2023-11-21 12:34:05 +0530

    Add files via upload

--- portfolio_template-master/src/components/About.js
+++ portfolio_template-master/src/components/About.js
@@ -1,19 +1,16 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/Profile2.jpg';
 
 const About = () => {
     return (
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+            Microsoft Certified Full Stack Developer/Cloud Architect ,Progressive experience in architecture design,development,deployment and management of on prim and cloud scenarios with minimal effort and well defined reusable components.
+            I currently serve as the Cloud Application Architect/Manager at MNC. Over the years, I've had the privilege of leading dynamic teams in the design and development of innovative solutions within the .NET framework.
+As we navigate the ever-evolving landscape of technology, I am excited to share that I am keenly interested in exploring the integration of blockchain technology into our projects. Recognizing the transformative potential of blockchain across various industries, I believe that merging our .NET expertise with blockchain capabilities can unlock new dimensions of efficiency, transparency, and security.
+While my background primarily lies in .NET development, I am eager to collaborate and learn alongside professionals like yourself who bring a wealth of experience in blockchain development. I see this as a valuable opportunity for cross-disciplinary collaboration, where our combined strengths can lead to groundbreaking solutions.
+
             </p>
         </section>
     );
--- portfolio_template-master/src/components/Header.js
+++ portfolio_template-master/src/components/Header.js
@@ -1,13 +1,13 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/Profile2.jpg';
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Kandes S" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm Kandes</h1>
                 <p>Blockchain Developer</p>
                 <button className='button'>Get In Touch</button>
             </div>
